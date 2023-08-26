d4298bba4af79a3dad3a67d12fb6dc372bc5c9e5
chojs23 <chojs990222@gmail.com>
2023-08-26 20:00:02 +0900

    create sub

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,22 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
+}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, prisma_client),
@@ -26,15 +42,10 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     prisma_client: web::Data<PrismaClient>,
 ) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name.clone()) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&new_subscriber, &prisma_client).await {
         Ok(_) => HttpResponse::Ok().finish(),
